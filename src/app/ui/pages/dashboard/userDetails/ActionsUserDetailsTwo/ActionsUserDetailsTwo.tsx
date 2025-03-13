9a97f9128ceb366f78a73501a9bfd1dfc7e4e998
Abdelrahman Ahmed Saad <abdelrahman9789@gmail.com>
2025-03-13 20:55:08 +0200

    delete clear user trainsaction button

--- src/app/ui/pages/dashboard/userDetails/ActionsUserDetailsTwo/ActionsUserDetailsTwo.tsx
+++ src/app/ui/pages/dashboard/userDetails/ActionsUserDetailsTwo/ActionsUserDetailsTwo.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { IconClearUser, IconDeleteUser } from "@/src/app/icons/icons";
 import { blockUser } from "@/src/util/blockUser";
@@ -8,27 +8,35 @@ import { UnblockUser } from "@/src/util/UnblockUser";
 import { useTranslations } from "next-intl";
 import { useParams } from "next/navigation";
 
-export default function ActionsUserDetailsTwo({status}:{status:string}) {
-  const t = useTranslations('dashboard')
-  const { user } = useParams()
+export default function ActionsUserDetailsTwo({ status }: { status: string }) {
+  const t = useTranslations("dashboard");
+  const { user } = useParams();
   return (
     <div className="flex flex-col gap-5">
-      <button onClick={()=>deleteUserTransactions(user as string)} className="flex items-center text-lg font-medium px-4 w-fit border border-[#B83131] rounded-full bg-[#FFEAEA] text-[#B83131] gap-2">
+      {/* <button onClick={()=>deleteUserTransactions(user as string)} className="flex items-center text-lg font-medium px-4 w-fit border border-[#B83131] rounded-full bg-[#FFEAEA] text-[#B83131] gap-2">
         <IconClearUser /> {t('clear-user-transactions')}
+      </button> */}
+      <button
+        onClick={() => deleteUser(user as string)}
+        className="flex items-center text-lg font-medium px-4 w-fit border border-[#B83131] rounded-full bg-[#FFEAEA] text-[#B83131] gap-2"
+      >
+        <IconDeleteUser /> {t("delete-user")}
       </button>
-      <button onClick={()=>deleteUser(user as string)} className="flex items-center text-lg font-medium px-4 w-fit border border-[#B83131] rounded-full bg-[#FFEAEA] text-[#B83131] gap-2">
-        <IconDeleteUser /> {t('delete-user')}
-      </button>
-      {
-        status === "active" ? 
-        <button onClick={()=>blockUser(user as string)} className="flex items-center text-lg font-medium px-4 w-fit border border-[#B83131] rounded-full bg-[#FFEAEA] text-[#B83131] gap-2">
-          <IconDeleteUser /> {t('ban-user')}
+      {status === "active" ? (
+        <button
+          onClick={() => blockUser(user as string)}
+          className="flex items-center text-lg font-medium px-4 w-fit border border-[#B83131] rounded-full bg-[#FFEAEA] text-[#B83131] gap-2"
+        >
+          <IconDeleteUser /> {t("ban-user")}
         </button>
-        :
-        <button onClick={()=>UnblockUser(user as string)} className="flex items-center text-lg font-medium px-4 w-fit border border-[#B83131] rounded-full bg-[#FFEAEA] text-[#B83131] gap-2">
-          <IconDeleteUser /> {t('un-ban-user')}
+      ) : (
+        <button
+          onClick={() => UnblockUser(user as string)}
+          className="flex items-center text-lg font-medium px-4 w-fit border border-[#B83131] rounded-full bg-[#FFEAEA] text-[#B83131] gap-2"
+        >
+          <IconDeleteUser /> {t("un-ban-user")}
         </button>
-      }
+      )}
     </div>
   );
 }
