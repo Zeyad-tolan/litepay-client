2880dd8a66a54bd5fe12d9084969b906942d1211
Abdelrahman Ahmed Saad <abdelrahman9789@gmail.com>
2025-03-16 11:09:40 +0200

    added hidden text in addbalance and edited transations in ar

--- src/app/ui/pages/ChargeCart/InputDollarChargeCart/InputDollarChargeCart.tsx
+++ src/app/ui/pages/ChargeCart/InputDollarChargeCart/InputDollarChargeCart.tsx
@@ -5,31 +5,39 @@ import Image from "next/image";
 import { ChangeEvent, Dispatch, SetStateAction } from "react";
 
 export type InputDollarChargeCartProps = {
-  dollarValue: number,
-  setDollarValue: Dispatch<SetStateAction<number>>,
-  setFocusInputName: Dispatch<SetStateAction<"" | "pound" | "dollar">>,
+  dollarValue: number;
+  setDollarValue: Dispatch<SetStateAction<number>>;
+  setFocusInputName: Dispatch<SetStateAction<"" | "pound" | "dollar">>;
 };
 
-export default function InputDollarChargeCart({ dollarValue, setDollarValue, setFocusInputName }: InputDollarChargeCartProps) {
-  const t = useTranslations("Cards")
+export default function InputDollarChargeCart({
+  dollarValue,
+  setDollarValue,
+  setFocusInputName,
+}: InputDollarChargeCartProps) {
+  const t = useTranslations("Cards");
 
   const handelChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setDollarValue(+e.target.value)
+    setDollarValue(+e.target.value);
     sessionStorage.setItem("dollar-value", `${+e.target.value}`);
-  }
+  };
 
   return (
     <div className="flex flex-col gap-1">
-      <p className="font-light">
-        {t("exactly")}
-      </p>
-      <div className={`flex gap-2 py-3 px-2 border border-solid ${dollarValue < 5 ? "border-amber-400" : "border-[#868685]"} rounded-xl`}>
+      <p className="font-light">{t("exactly")}</p>
+      <div
+        className={`flex gap-2 py-3 px-2 border border-solid ${
+          dollarValue < 5 ? "border-amber-400" : "border-[#868685]"
+        } rounded-xl`}
+      >
         <input
           type="text"
           required
           name="amountUsd"
           inputMode="numeric"
-          className={`outline-none bg-transparent flex-1 min-w-14 ${dollarValue < 5 ? "text-amber-400" : ""}`}
+          className={`outline-none bg-transparent flex-1 min-w-14 ${
+            dollarValue < 5 ? "text-amber-400" : ""
+          }`}
           value={Math.floor(dollarValue)}
           onChange={(e) => handelChange(e)}
           onFocus={() => setFocusInputName("dollar")}
@@ -41,14 +49,24 @@ export default function InputDollarChargeCart({ dollarValue, setDollarValue, set
           alt="dollar usa"
           className="w-6 h-6 rounded-full object-left-top"
         />
-        <p>
-          {t("usd")}
-        </p>
+        <p>{t("usd")}</p>
       </div>
       <p className="font-light">
-        {!Number.isInteger(dollarValue) && t('the-platform-only-accepts-whole-numbers').concat(" ", `${dollarValue.toFixed(2)}`, " ", t('will-be-rounded-to'), " ", `${Math.floor(dollarValue)}`)}
+        {!Number.isInteger(dollarValue) &&
+          t("the-platform-only-accepts-whole-numbers").concat(
+            " ",
+            `${dollarValue.toFixed(2)}`,
+            " ",
+            t("will-be-rounded-to"),
+            " ",
+            `${Math.floor(dollarValue)}`
+          )}
       </p>
-      <p className={`text-sm font-light ${dollarValue < 5 ? "text-amber-400" : ""}`}>
+      <p
+        className={`text-sm font-light ${
+          dollarValue < 5 ? "text-amber-400" : ""
+        }`}
+      >
         {t("minimum")} : 5$
       </p>
     </div>
--- src/app/ui/pages/addBalance/HeroAddBalance/HeroAddBalance.tsx
+++ src/app/ui/pages/addBalance/HeroAddBalance/HeroAddBalance.tsx
@@ -1,13 +1,12 @@
 import { useTranslations } from "next-intl";
 
 export default function HeroAddBalance() {
-  const t = useTranslations("Cards")
+  const t = useTranslations("Cards");
 
   return (
     <div className="w-full flex flex-col gap-1 items-center text-center">
-      <p className="text-4xl font-medium">
-        {t("charge-card")}
-      </p>
+      <p className="text-4xl font-medium">{t("charge-card")}</p>
+      <p className="text-4xl font-medium hidden">charge card</p>
     </div>
   );
 }
