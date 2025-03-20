c253c9fd24513118b923a8fd9c1312a4c5e26d3a
Abdelrahman Ahmed Saad <abdelrahman9789@gmail.com>
2025-03-20 21:28:53 +0200

    Enhance TypeCharge component with toast notifications for unavailable Vodafone Cash option; add translations for error messages in Arabic and English.

--- src/app/ui/pages/ChargeCart/TypeCharge/TypeCharge.tsx
+++ src/app/ui/pages/ChargeCart/TypeCharge/TypeCharge.tsx
@@ -1,32 +1,48 @@
-import { RadioActiveIcon, RadioInactiveIcon, VodafoneCashIcon } from "@/src/app/icons/icons";
+import {
+  RadioActiveIcon,
+  RadioInactiveIcon,
+  VodafoneCashIcon,
+} from "@/src/app/icons/icons";
 import srcImage from "@/src/shared/insta-pay-image.png";
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 import { Dispatch, SetStateAction } from "react";
+import toast from "react-hot-toast";
 
 export type TypeChargeProps = {
-  setValue: Dispatch<SetStateAction<"instapay" | "vodafone">>
-  value: "instapay" | "vodafone",
-  setFocusInputName: Dispatch<SetStateAction<"" | "pound" | "dollar">>,
+  setValue: Dispatch<SetStateAction<"instapay" | "vodafone">>;
+  value: "instapay" | "vodafone";
+  setFocusInputName: Dispatch<SetStateAction<"" | "pound" | "dollar">>;
 };
 
-export default function TypeCharge({ value, setValue, setFocusInputName }: TypeChargeProps) {
+export default function TypeCharge({
+  value,
+  setValue,
+  setFocusInputName,
+}: TypeChargeProps) {
+  const t = useTranslations("Cards");
   return (
     <div className="flex flex-col">
-      <div className="flex justify-between items-center cursor-pointer" onClick={() => { setValue("instapay"); setFocusInputName("dollar") }}>
+      <div
+        className="flex justify-between cursor-pointer items-center"
+        onClick={() => {
+          setValue("instapay");
+          setFocusInputName("dollar");
+        }}
+      >
         {value == "instapay" ? <RadioActiveIcon /> : <RadioInactiveIcon />}
-        <div className="flex-1 flex justify-center">
-          <Image
-            src={srcImage}
-            alt="insta-pay"
-            className="h-20 w-fit"
-          />
+        <div className="flex flex-1 justify-center">
+          <Image src={srcImage} alt="insta-pay" className="h-20 w-fit" />
         </div>
       </div>
-      <div className="flex justify-between items-center cursor-not-allowed opacity-50"
-      // onClick={() => { setValue("vodafone"); setFocusInputName("dollar") }}
+      <div
+        className="flex justify-between cursor-not-allowed items-center opacity-50"
+        onClick={() => {
+          toast.error(t("vodafone-unavailable"));
+        }}
       >
         {value == "vodafone" ? <RadioActiveIcon /> : <RadioInactiveIcon />}
-        <div className="flex-1 flex justify-center">
+        <div className="flex flex-1 justify-center">
           <VodafoneCashIcon />
         </div>
       </div>
