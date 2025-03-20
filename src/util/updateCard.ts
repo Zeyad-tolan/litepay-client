a53cfe37737a069d26b8df7e0788e0a57c7d76c0
Abdelrahman Ahmed Saad <abdelrahman9789@gmail.com>
2025-03-20 21:46:25 +0200

    Refactor updateCard function to include userId in request body and improve error handling; enhance readability with consistent formatting.

--- src/util/updateCard.ts
+++ src/util/updateCard.ts
@@ -1,38 +1,42 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import Cookies from "js-cookie"
-import { Dispatch, SetStateAction } from "react"
-import toast from "react-hot-toast"
+import Cookies from "js-cookie";
+import { Dispatch, SetStateAction } from "react";
+import toast from "react-hot-toast";
 
-export const updateCard = (id:string, data:any,setShow?: Dispatch<SetStateAction<boolean>>)=>{
-    return fetch(`${process.env.NEXT_PUBLIC_API_URL}cards/${id}`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-            "token": `${Cookies.get("token")}`,
-        },
-        body: JSON.stringify({
-            ...data
-        })
+export const updateCard = (
+  id: string,
+  data: any,
+  setShow?: Dispatch<SetStateAction<boolean>>
+) => {
+  return fetch(`${process.env.NEXT_PUBLIC_API_URL}cards/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      token: `${Cookies.get("token")}`,
+    },
+    body: JSON.stringify({
+      ...data,
+      userId: Cookies.get("id"),
+    }),
+  })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        return res.json().then((errorData) => {
+          throw new Error(errorData.message || "An error occurred");
+        });
+      }
     })
-    .then(res=>{
-        if(res.ok){
-            return res.json()
-        }
-        else{
-            return res.json().then((errorData) => {
-                throw new Error(errorData.message || 'An error occurred');
-            });
-        }
+    .then((data) => {
+      // console.log(data)
+      toast.success(data.message);
+      if (setShow) {
+        setShow(false);
+      }
     })
-        .then(data => {
-            // console.log(data)
-            toast.success(data.message)
-            if(setShow){
-                setShow(false)
-            }
-        })
-        .catch(err => {
-            console.log(err)
-            toast.error(err.message)
-        })
-}
+    .catch((err) => {
+      console.log(err);
+      toast.error(err.message);
+    });
+};
