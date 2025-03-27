7b2811e781390dd0550c4f807b304207707bd98b
Abdelrahman Ahmed Saad <abdelrahman9789@gmail.com>
2025-03-27 12:18:03 +0200

    Update QrDetailsInstaPay component with new image source and updated link; remove unused image file.

--- src/app/ui/pages/instapay/QrDetailsInstaPay/QrDetailsInstaPay.tsx
+++ src/app/ui/pages/instapay/QrDetailsInstaPay/QrDetailsInstaPay.tsx
@@ -1,23 +1,22 @@
-import srcImage from "@/src/shared/qr-instapay.jpeg";
+import srcImage from "@/src/shared/qr-instapay.jpg";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function QrDetailsInstaPay() {
-  const t = useTranslations("Cards")
-  const link = "https://ipn.eg/S/zeyadtolan/instapay/4HxTOj"
-  const accountInstaPay = "zeyadtolan@instapay"
+  const t = useTranslations("Cards");
+  const link = "https://ipn.eg/S/zeyadtolanfab/instapay/7qZJzj";
+  const accountInstaPay = "zeyadtolanfab@instapay";
 
   return (
     <div className="w-full flex flex-col gap-3 items-center">
-      <Image
-        src={srcImage}
-        alt="qr"
-        className="w-52"
-      />
+      <Image src={srcImage} alt="qr" className="w-52" />
       <div className="flex flex-col gap-1 items-center">
         <p>
-          {t("Link")} : <Link href={link} target="_blank" className="text-blue-400 underline">{t("click-instaPay")}</Link>
+          {t("Link")} :{" "}
+          <Link href={link} target="_blank" className="text-blue-400 underline">
+            {t("click-instaPay")}
+          </Link>
         </p>
         <p>
           {t("user")} : {accountInstaPay}
