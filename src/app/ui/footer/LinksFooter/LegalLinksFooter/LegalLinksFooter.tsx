48bd6680a1c9c8b5a69dba06401e4e41c7efdfb1
Abdelrahman Ahmed Saad <abdelrahman9789@gmail.com>
2025-03-31 07:25:46 +0200

    adding pdfs of legals

--- src/app/ui/footer/LinksFooter/LegalLinksFooter/LegalLinksFooter.tsx
+++ src/app/ui/footer/LinksFooter/LegalLinksFooter/LegalLinksFooter.tsx
@@ -6,25 +6,26 @@ import Link from "next/link";
 import { useParams } from "next/navigation";
 
 export default function LegalLinksFooter() {
-  const t = useTranslations("Footer")
-  const { lang } = useParams()
+  const t = useTranslations("Footer");
+  const { lang } = useParams();
   return (
     <div className="flex flex-col gap-4 col-span-2">
-      <p className="font-semibold text-primary">
-        {t("legal")}
-      </p>
+      <p className="font-semibold text-primary">{t("legal")}</p>
       <div className="flex sm:flex-col flex-row gap-2">
-        {
-          footerLinks.legal.map(({ link, title }, index) => {
-            return (
-              <Link
-                className="text-white font-medium"
-                href={"/" + lang + link} key={index}>
-                {t(title)}
-              </Link>
-            )
-          })
-        }
+        {footerLinks.legal.map(({ link, title }, index) => {
+          const isPDF = link.endsWith(".pdf");
+          return (
+            <Link
+              className="text-white font-medium"
+              href={link}
+              key={index}
+              target={isPDF ? "_blank" : undefined}
+              rel={isPDF ? "noopener noreferrer" : undefined}
+            >
+              {t(title)}
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
