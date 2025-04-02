6f7f406e4e955d826df7657ed5735756424b92ae
Abdelrahman Ahmed Saad <abdelrahman9789@gmail.com>
2025-04-02 21:09:46 +0200

    disabling charge new card

--- src/app/ui/pages/ChargeCart/FormChargeCard/FormChargeCard.tsx
+++ src/app/ui/pages/ChargeCart/FormChargeCard/FormChargeCard.tsx
@@ -17,81 +17,133 @@ import TypeCharge from "../TypeCharge";
 import { jwtDecode } from "jwt-decode";
 import Cookies from "js-cookie";
 import { getCardPrice } from "@/src/util/getCardPrice";
+import toast from "react-hot-toast";
 
 export default function FormChargeCard() {
-  const [typeCharge, setTypeCharge] = useState<"instapay" | "vodafone">("instapay")
-  const [currencyDifference, setCurrencyDifference] = useState<number>(51)
-  const [dollarValue, setDollarValue] = useState<number>(1)
-  const [poundValue, setPoundValue] = useState<number>(51)
-  const [focusInputName, setFocusInputName] = useState<"" | "pound" | "dollar">("")
-  const [disabled, setDisabled] = useState<boolean>(true)
-  const [promoCodeValue, setPromoCodeValue] = useState<string>("")
-  const [instapayValue, setInstapayValue] = useState<number>(0)
-  const [vodafoneCash, setVodafoneCash] = useState<number>(0)
-  const [normalUser, setNormalUser] = useState<number>(0)
-  const [vipUser, setVipUser] = useState<number>(0)
-  const [priceCard, setCardPrice] = useState<number>(5)
-  const router = useRouter()
-  const { lang } = useParams()
-  const { user }:any = jwtDecode(Cookies.get("token") as string);
+  const [typeCharge, setTypeCharge] = useState<"instapay" | "vodafone">(
+    "instapay"
+  );
+  const [currencyDifference, setCurrencyDifference] = useState<number>(51);
+  const [dollarValue, setDollarValue] = useState<number>(1);
+  const [poundValue, setPoundValue] = useState<number>(51);
+  const [focusInputName, setFocusInputName] = useState<"" | "pound" | "dollar">(
+    ""
+  );
+  const [disabled, setDisabled] = useState<boolean>(true);
+  const [promoCodeValue, setPromoCodeValue] = useState<string>("");
+  const [instapayValue, setInstapayValue] = useState<number>(0);
+  const [vodafoneCash, setVodafoneCash] = useState<number>(0);
+  const [normalUser, setNormalUser] = useState<number>(0);
+  const [vipUser, setVipUser] = useState<number>(0);
+  const [priceCard, setCardPrice] = useState<number>(5);
+  const router = useRouter();
+  const { lang } = useParams();
+  const { user }: any = jwtDecode(Cookies.get("token") as string);
 
-  const instaFee = instapayValue
-  const vodafoneCashFee = vodafoneCash
-  const commission = user.Role ? user.Role.type === "user" ? normalUser : vipUser : normalUser
+  const instaFee = instapayValue;
+  const vodafoneCashFee = vodafoneCash;
+  const commission = user.Role
+    ? user.Role.type === "user"
+      ? normalUser
+      : vipUser
+    : normalUser;
   // console.log(commission);
 
   useEffect(() => {
-    getExchangeRate(setCurrencyDifference)
-    getAllRating(setInstapayValue, setVodafoneCash, setNormalUser, setVipUser)
-    getCardPrice(setCardPrice)
-    const dollar = sessionStorage.getItem("dollar-value")
-    dollar && setDollarValue(+dollar)
-    const pound = sessionStorage.getItem("pound-value")
-    pound && setPoundValue(+pound)
-  }, [])
+    getExchangeRate(setCurrencyDifference);
+    getAllRating(setInstapayValue, setVodafoneCash, setNormalUser, setVipUser);
+    getCardPrice(setCardPrice);
+    const dollar = sessionStorage.getItem("dollar-value");
+    dollar && setDollarValue(+dollar);
+    const pound = sessionStorage.getItem("pound-value");
+    pound && setPoundValue(+pound);
+  }, []);
 
   useEffect(() => {
     const baseValue = dollarValue * currencyDifference;
     const fees = baseValue * commission;
-    const cardPrice = priceCard * currencyDifference
-    const additionalCharge = typeCharge === "vodafone" ? (baseValue + fees) * vodafoneCashFee : instaFee;
+    const cardPrice = priceCard * currencyDifference;
+    const additionalCharge =
+      typeCharge === "vodafone"
+        ? (baseValue + fees) * vodafoneCashFee
+        : instaFee;
 
     if (focusInputName == "dollar") {
-      setPoundValue(baseValue + fees + additionalCharge + cardPrice)
-      sessionStorage.setItem("pound-value", `${Math.ceil(baseValue + fees + additionalCharge + cardPrice)}`)
+      setPoundValue(baseValue + fees + additionalCharge + cardPrice);
+      sessionStorage.setItem(
+        "pound-value",
+        `${Math.ceil(baseValue + fees + additionalCharge + cardPrice)}`
+      );
     }
 
     if (focusInputName == "pound") {
       setDollarValue(
-        typeCharge === "vodafone" ?
-          ((poundValue - cardPrice) / (currencyDifference * (1 + commission + vodafoneCashFee))) :
-          ((poundValue - cardPrice) / (currencyDifference * (1 + commission + instaFee)))
+        typeCharge === "vodafone"
+          ? (poundValue - cardPrice) /
+              (currencyDifference * (1 + commission + vodafoneCashFee))
+          : (poundValue - cardPrice) /
+              (currencyDifference * (1 + commission + instaFee))
       );
-      sessionStorage.setItem("dollar-value", `${Math.floor(
-        typeCharge === "vodafone" ?
-          ((poundValue - cardPrice) / (currencyDifference * (1 + commission + vodafoneCashFee))) :
-          ((poundValue - cardPrice) / (currencyDifference * (1 + commission + instaFee)))
-      )}`)
+      sessionStorage.setItem(
+        "dollar-value",
+        `${Math.floor(
+          typeCharge === "vodafone"
+            ? (poundValue - cardPrice) /
+                (currencyDifference * (1 + commission + vodafoneCashFee))
+            : (poundValue - cardPrice) /
+                (currencyDifference * (1 + commission + instaFee))
+        )}`
+      );
     }
 
     if (dollarValue >= 5 || poundValue >= 50000) {
-      setDisabled(false)
+      setDisabled(false);
     } else {
-      setDisabled(true)
+      setDisabled(true);
     }
-
-  }, [dollarValue, poundValue, typeCharge,priceCard])
+  }, [dollarValue, poundValue, typeCharge, priceCard]);
   return (
     <form
       className="w-full flex flex-col gap-8"
-      onSubmit={(e) => handelSubmitChargeCard(e, `${currencyDifference}`, promoCodeValue, typeCharge, lang?.toString() as string, router)}
+      onSubmit={(e) => {
+        toast.error("This feature is under maintenance");
+        return;
+
+        handelSubmitChargeCard(
+          e,
+          `${currencyDifference}`,
+          promoCodeValue,
+          typeCharge,
+          lang?.toString() as string,
+          router
+        );
+      }}
     >
       <div className="w-full flex flex-col gap-2">
-        <InputDollarChargeCart dollarValue={dollarValue} setDollarValue={setDollarValue} setFocusInputName={setFocusInputName} />
-        <DetailsChargeCart cardPrice={priceCard} commission={commission} instapay={instapayValue} vodafoneCash={vodafoneCash} chargeType={typeCharge} currencyDifference={currencyDifference} />
-        <InputPoundsChargeCart poundValue={poundValue} setPoundValue={setPoundValue} setFocusInputName={setFocusInputName} />
+        <InputDollarChargeCart
+          dollarValue={dollarValue}
+          setDollarValue={setDollarValue}
+          setFocusInputName={setFocusInputName}
+        />
+        <DetailsChargeCart
+          cardPrice={priceCard}
+          commission={commission}
+          instapay={instapayValue}
+          vodafoneCash={vodafoneCash}
+          chargeType={typeCharge}
+          currencyDifference={currencyDifference}
+        />
+        <InputPoundsChargeCart
+          poundValue={poundValue}
+          setPoundValue={setPoundValue}
+          setFocusInputName={setFocusInputName}
+        />
         <PromoCode setValue={setPromoCodeValue} value={promoCodeValue} />
-        <TypeCharge setFocusInputName={setFocusInputName} setValue={setTypeCharge} value={typeCharge} />
+        <TypeCharge
+          setFocusInputName={setFocusInputName}
+          setValue={setTypeCharge}
+          value={typeCharge}
+        />
       </div>
       <SubmitBtn title="next" disabled={disabled} />
     </form>
